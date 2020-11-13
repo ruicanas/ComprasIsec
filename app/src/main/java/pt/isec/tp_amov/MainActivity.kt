fa80ce012dcfaf7ac7da89314b8b42ff7f4dfd1c
Rui Canas <ruijfcanas@gmail.com>
2020-11-13 16:08:24 +0000

    Updated Test MainActivity.kt

--- app/src/main/java/pt/isec/tp_amov/MainActivity.kt
+++ app/src/main/java/pt/isec/tp_amov/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var TAG = "Test"
+        val TAG = "Test2"
         Log.i(TAG, "This is a test")
     }
 }
