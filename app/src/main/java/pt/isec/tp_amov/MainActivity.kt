225b2ad978a63688f3b7e044005e6a0209eeaf55
Gabriel Gomes <gabrielqgomes46@gmail.com>
2020-11-13 15:33:19 +0000

    Test commit

--- app/src/main/java/pt/isec/tp_amov/MainActivity.kt
+++ app/src/main/java/pt/isec/tp_amov/MainActivity.kt
@@ -2,10 +2,14 @@ package pt.isec.tp_amov
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var TAG = "Test"
+        Log.i(TAG, "onCreate: ")
     }
 }
