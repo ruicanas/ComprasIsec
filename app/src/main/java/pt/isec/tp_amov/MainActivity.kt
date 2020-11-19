299f7237196956f1d5ef447217a69d53b4dde59f
Rui Canas <ruijfcanas@gmail.com>
2020-11-19 01:43:59 +0000

    [NEW]: Main Interface

--- app/src/main/java/pt/isec/tp_amov/MainActivity.kt
+++ app/src/main/java/pt/isec/tp_amov/MainActivity.kt
@@ -3,13 +3,13 @@ package pt.isec.tp_amov
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var TAG = "Test"
-        Log.i(TAG, "[RESOLVED]This is a test")
     }
+
+
 }
