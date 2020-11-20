59a3484c8b110d569a428b5aba7121c84338ef4e
Gabriel Gomes <gabrielqgomes46@gmail.com>
2020-11-20 16:54:06 +0000

    Nearly complete new product page. Added category and unit strings files.

--- app/src/main/java/pt/isec/tp_amov/ManageProductActivity.kt
+++ app/src/main/java/pt/isec/tp_amov/ManageProductActivity.kt
@@ -4,14 +4,46 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 /**
  * This activity is going to be responsible for the creation and edition of a product
  */
-class ManageProductActivity : AppCompatActivity() {
+class ManageProductActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage_product)
+
+        val spinnerC: Spinner = findViewById(R.id.spinnerCat)
+        val spinnerU: Spinner = findViewById(R.id.spinnerUnit)
+
+        //create array adapter for the spinner
+        ArrayAdapter.createFromResource(this, R.array.category_array, android.R.layout.simple_spinner_item).also {
+            adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerC.adapter = adapter
+        }
+
+        //create array adapter for the spinner
+        ArrayAdapter.createFromResource(this, R.array.unit_array, android.R.layout.simple_spinner_item).also {
+            adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerU.adapter = adapter
+        }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        //setup listener for on spinner item selected
+        val spinner: Spinner = findViewById(R.id.spinnerCat)
+        spinner.onItemSelectedListener = this
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
+        
     }
 
 
